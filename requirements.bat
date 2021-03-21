1553ee6eb3745ee5213b81f84af9c9dcfdff6276
Mohammad Abu Kowser Shakil <42375657+shakil2510@users.noreply.github.com>
2021-03-22 00:45:06 +0600

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -9,4 +9,6 @@ pip install colored
 :: sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
 :: pip install colored
 
+timeout 3
 
+pause
