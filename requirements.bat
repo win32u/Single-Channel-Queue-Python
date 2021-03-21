88255b484b1db88931edee71c7e0f761a5b52ae6
Mohammad Abu Kowser Shakil <42375657+shakil2510@users.noreply.github.com>
2021-03-21 23:38:52 +0600

    Create requirements.bat

---
+++ requirements.bat
@@ -0,0 +1,12 @@
+:: windows
+
+echo 'Installing Python Libraries:'
+echo 'Please Waint...'
+python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+pip install colored
+
+:: Linux(Ubuntu and Debian)
+:: sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+:: pip install colored
+
+
